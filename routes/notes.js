ffaf337b71ef6eed27c8bb2a505c90eb4f94b080
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-07-19 17:54:32 -0400

    have the intiial get and push routes working

--- routes/notes.js
+++ routes/notes.js
@@ -1,38 +1,37 @@
-const fb = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const nt = require("express").Router();
+// const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
 // GET Route for retrieving all the feedback
-fb.get('/', (req, res) =>
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+nt.get("/", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
 
-// POST Route for submitting feedback
-fb.post('/', (req, res) => {
+// POST Route for submitting note
+nt.post("/", (req, res) => {
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+  const { title, text } = req.body;
 
   // If all the required properties are present
-  if (email && feedbackType && feedback) {
+  if (title && text) {
     // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuidv4(),
+    const newNote = {
+      title,
+      text,
+      // id: uuidv4()
     };
 
-    readAndAppend(newFeedback, './db/feedback.json');
+    readAndAppend(newNote, "./db/db.json");  //should this be ../db/db.json?
 
     const response = {
-      status: 'success',
-      body: newFeedback,
+      status: "success",
+      body: newNote,
     };
 
     res.json(response);
   } else {
-    res.json('Error in posting feedback');
+    res.json("Failed to post note.");
   }
 });
 
-module.exports = fb;
+module.exports = nt;
--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const fs = require("fs/promises");
 const api = require('./routes/index.js');
+
 // const { <process name> } = require("<./helpers/filename>");
 // app.use(<constant from above>)
 
-const PORT = process.env.PORT || 3001; //this opens port 3001 and ...
+const PORT = process.env.PORT || 3002; //this opens port 3001 and ...
 const app = express(); //this assigns "app" to express
 
 //middleware
 app.use(express.json()); //processing json files
 app.use(express.urlencoded({ extended: true })); //handling urls
-app.use("./api", api);
-
+app.use("/api", api); //sets up a router to handle requests that being with "api" (which you can see are called in /public/assets/js/index.js)
 app.use(express.static("public")); //tell app where to look for files not in the root directory
 
-//routes
+//get route for home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//move these into the routes folder
-app.get("./notes"), (req, res) => { //return notes.html file
-  res.sendFile(path.join(__dirname, "./public.notes.html"));
-};
+//get route for notes file
+app.get("/notes", (req, res) => { //return notes.html file
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-app.get("./api/notes"), (req, res) => { //return db.json file
+//in this context, ".api" is addresses a router object, not an actual folder on my hard drive
+//think of it as a "watched" folder that executes some function when something gets dropped into it
+app.get("/api/notes"), (req, res) => { //return db.json file
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 };
 
-app.post("./api/notes"), (req, res) => { //post new note to db.json
-//get real variables for this
-  const { email, feedbackType, feedback } = req.body;
+app.post("/api/notes"), (req, res) => { //post new note to db.json 
+  const { title, text,  } = req.body;
 };
 
-//optional
-//app.delete("./api/notes/:id"), (req, res) => { //optional to delete note from the db and the note iteself
-//   res.delete
-// };
+// //optional
+// //app.delete("./api/notes/:id"), (req, res) => { //optional to delete note from the db and the note iteself
+// //   res.delete
+// // };
 
-app.get("./*"), (req, res) => { //return index.html file
-  app.get
-}
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+// app.get("./*"), (req, res) => { //return index.html file
+//   app.get
+// }
+app.listen(PORT, () => { 
+  console.log(`App listening on port ${PORT}`)
+})
